996c1af878c18af99b15396c61e14bbf6faff464
Brody McLemore <McLeBro@bvu.edu>
2022-12-27 22:10:29 -0600

    Formatting code to pep8 standards

--- App.py
+++ App.py
@@ -6,87 +6,95 @@ from riotwatcher import LolWatcher
 from Project_Files.config import API_KEY
 
 
-## Top Level Window handling
+# Top Level Window handling
 # Making the Window
 MainWin = Tk()
 MainWin.title("LOL Breakdown")
-MainWin.resizable(width=False,height=False)
+MainWin.resizable(width=False, height=False)
 MainWin.geometry("300x200")
 MainWin.iconbitmap("Project_Files\Mascot.ico")
 
-# Handling the Frame 
+# Handling the Frame
 MainFrame = Frame(MainWin)
-MainFrame.pack(padx=10,pady=10)
-MainFrame.grid(column=0,row=0,sticky="NW")
+MainFrame.pack(padx=10, pady=10)
+MainFrame.grid(column=0, row=0, sticky="NW")
 
-## Setting Global Variables 
+# Setting Global Variables
 # App Variables
 PlayerName = StringVar()
 RegionVal = StringVar()
 
 # Working Variables
 RegionCommon = [
-    "North America", 
-    "Brazil", 
-    "Europe Nordic & East", 
-    "Europe West", 
-    "Latin America North", 
-    "Latin America South", 
+    "North America",
+    "Brazil",
+    "Europe Nordic & East",
+    "Europe West",
+    "Latin America North",
+    "Latin America South",
     "Oceania",
     "Russia",
     "Turkey",
     "Japan"
 ]
 RegionServer = {
-    "North America" : "na1",
-    "Brazil" : "br1",
-    "Europe Nordic & East" : "eun1",
-    "Europe West" : "euw1",
-    "Latin America North" : "la1",
-    "Latin America South" : "la2",
-    "Oceania" : "oc1",
-    "Russia" : "ru",
-    "Turkey" : "tr1",
-    "Japan" : "jp1"
+    "North America": "na1",
+    "Brazil": "br1",
+    "Europe Nordic & East": "eun1",
+    "Europe West": "euw1",
+    "Latin America North": "la1",
+    "Latin America South": "la2",
+    "Oceania": "oc1",
+    "Russia": "ru",
+    "Turkey": "tr1",
+    "Japan": "jp1"
 }
-CallValues = ["summonerName","championName","teamPosition","kills","totalDamageDealt", "goldEarned", "visionScore","win"]
+CallValues = ["summonerName", "championName", "teamPosition",
+              "kills", "totalDamageDealt", "goldEarned", "visionScore", "win"]
 GameCount = 0
 Row = 1
 watcher = LolWatcher(API_KEY)
 
-## Functions and Backend
+# Functions and Backend
+
+
 def Run():
     Region = RegionServer[RegionVal.get()]
     AccountName = PlayerName.get()
     AccountInfo = watcher.summoner.by_name(Region, AccountName)
     MatchList = watcher.match.matchlist_by_puuid(Region, AccountInfo["puuid"])
     wb = Workbook()
-    OutputGames(Region,MatchList,GameCount,Row,CallValues,watcher,wb)
-    if SaveBook(wb,AccountName) == "Success":
-        messagebox.showinfo("Success", "Your Game Data has been saved to a new folder named 'Output'.")
-    elif SaveBook(wb,AccountName) == "Fail":
-        messagebox.showerror("Failure", "Your Game Data did NOT save correctly, please try again")
+    OutputGames(Region, MatchList, GameCount, Row, CallValues, watcher, wb)
+    if SaveBook(wb, AccountName) == "Success":
+        messagebox.showinfo(
+            "Success", "Your Game Data has been saved to a new folder named 'Output'.")
+    elif SaveBook(wb, AccountName) == "Fail":
+        messagebox.showerror(
+            "Failure", "Your Game Data did NOT save correctly, please try again")
 
 
-
-## Building the Front End 
+# Building the Front End
 # Labels, Input boxes, and Submit Buttons
-PlayerNameLabel = ttk.Label(MainFrame, text = "Player Name:", font=('calibre',10, 'bold'),anchor="center")
-RegionLabel = ttk.Label(MainFrame, text = "Region:", font=('calibre',10, 'bold'),justify="left",anchor="center")
-PlayerNameEntry = ttk.Entry(MainFrame, textvariable=PlayerName,font=('calibre',10,'normal'))
-RegionComboBox = ttk.Combobox(MainFrame,textvariable=RegionVal,values=RegionCommon,state="readonly",font=('calibre',10,'normal'))
-SubmitButton = ttk.Button(MainFrame,text="Submit",command=Run)
+PlayerNameLabel = ttk.Label(MainFrame, text="Player Name:", font=(
+    'calibre', 10, 'bold'), anchor="center")
+RegionLabel = ttk.Label(MainFrame, text="Region:", font=(
+    'calibre', 10, 'bold'), justify="left", anchor="center")
+PlayerNameEntry = ttk.Entry(
+    MainFrame, textvariable=PlayerName, font=('calibre', 10, 'normal'))
+RegionComboBox = ttk.Combobox(MainFrame, textvariable=RegionVal,
+                              values=RegionCommon, state="readonly", font=('calibre', 10, 'normal'))
+SubmitButton = ttk.Button(MainFrame, text="Submit", command=Run)
 
 # Widget Positions
-PlayerNameLabel.grid(column=1,row=2,pady=5)
-PlayerNameEntry.grid(column=2,row=2)
-RegionLabel.grid(column=1,row=3,pady=5)
-RegionComboBox.grid(column=2,row=3,)
-SubmitButton.grid(column=1,row=4,pady=5,columnspan=2)
+PlayerNameLabel.grid(column=1, row=2, pady=5)
+PlayerNameEntry.grid(column=2, row=2)
+RegionLabel.grid(column=1, row=3, pady=5)
+RegionComboBox.grid(column=2, row=3,)
+SubmitButton.grid(column=1, row=4, pady=5, columnspan=2)
 
 # Setting the Default value for the RegionComboBox to "North America (na)"
 RegionComboBox.set("North America")
 
 
-## Running the Window
+# Running the Window
 MainWin.mainloop()
--- Project_Files/Func.py
+++ Project_Files/Func.py
@@ -3,6 +3,7 @@ from openpyxl import Workbook
 from riotwatcher import LolWatcher
 import os
 
+
 def SaveBook(Workbook, PlayerName):
     wb = Workbook
     if not os.path.exists(os.getcwd()+"/Output"):
@@ -19,44 +20,53 @@ def SaveBook(Workbook, PlayerName):
         except:
             return "Fail"
 
-def OutputGames(Region,MatchList,GameCount,Row,CallValues,Watcher,Workbook):
+
+def OutputGames(Region, MatchList, GameCount, Row, CallValues, Watcher, Workbook):
     watcher = Watcher
     wb = Workbook
     while GameCount <= 17:
-        MatchDetail = watcher.match.by_id(Region,MatchList[GameCount])
+        MatchDetail = watcher.match.by_id(Region, MatchList[GameCount])
         if MatchDetail["info"]["gameMode"] == "CLASSIC":
-            MakeBoilerPlateCells(wb["Sheet"],Row,MatchDetail)
-            OutputRed(wb["Sheet"],MatchDetail,Row,CallValues)
-            OutputBlue(wb["Sheet"],MatchDetail,Row,CallValues)
-            Row +=8
-        GameCount +=1
+            MakeBoilerPlateCells(wb["Sheet"], Row, MatchDetail)
+            OutputRed(wb["Sheet"], MatchDetail, Row, CallValues)
+            OutputBlue(wb["Sheet"], MatchDetail, Row, CallValues)
+            Row += 8
+        GameCount += 1
 
-def OutputRed(worksheet,MatchDetail,startRow,CallValues):
+
+def OutputRed(worksheet, MatchDetail, startRow, CallValues):
     ws = worksheet
     StartWorkRow = startRow + 3
     for row in range(5):
         for col in range(len(CallValues)):
             if CallValues[col] != "kills":
-                CalledValue = ws.cell(StartWorkRow+row,col+1)
+                CalledValue = ws.cell(StartWorkRow+row, col+1)
                 CalledValue.value = MatchDetail["info"]['participants'][row][CallValues[col]]
             else:
-                CalledValue = ws.cell(StartWorkRow+row,col+1)
+                CalledValue = ws.cell(StartWorkRow+row, col+1)
                 Kills = str(MatchDetail["info"]['participants'][row]["kills"])
-                Deaths = str(MatchDetail["info"]['participants'][row]["deaths"])
-                Assists = str(MatchDetail["info"]['participants'][row]["assists"])
+                Deaths = str(MatchDetail["info"]
+                             ['participants'][row]["deaths"])
+                Assists = str(MatchDetail["info"]
+                              ['participants'][row]["assists"])
                 CalledValue.value = Kills + "/" + Deaths + "/" + Assists
 
-def OutputBlue(worksheet,MatchDetail,startRow,CallValues):
+
+def OutputBlue(worksheet, MatchDetail, startRow, CallValues):
     ws = worksheet
     StartWorkRow = startRow + 3
     for row in range(5):
         for col in range(len(CallValues)):
             if CallValues[col] != "kills":
-                CalledValue = ws.cell(StartWorkRow+row,col+2+len(CallValues))
-                CalledValue.value = MatchDetail["info"]['participants'][row+5][CallValues[col]]
+                CalledValue = ws.cell(StartWorkRow+row, col+2+len(CallValues))
+                CalledValue.value = MatchDetail["info"]['participants'][row +
+                                                                        5][CallValues[col]]
             else:
-                CalledValue = ws.cell(StartWorkRow+row,col+2+len(CallValues))
-                Kills = str(MatchDetail["info"]['participants'][row+5]["kills"])
-                Deaths = str(MatchDetail["info"]['participants'][row+5]["deaths"])
-                Assists = str(MatchDetail["info"]['participants'][row+5]["assists"])
+                CalledValue = ws.cell(StartWorkRow+row, col+2+len(CallValues))
+                Kills = str(MatchDetail["info"]
+                            ['participants'][row+5]["kills"])
+                Deaths = str(MatchDetail["info"]
+                             ['participants'][row+5]["deaths"])
+                Assists = str(MatchDetail["info"]
+                              ['participants'][row+5]["assists"])
                 CalledValue.value = Kills + "/" + Deaths + "/" + Assists
